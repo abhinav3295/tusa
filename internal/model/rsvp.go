6ea9d301e7626b0fe82efdf20242fcadb91107a7
Abhinav Singh Suryavanshi <abhinav3295@gmail.com>
2022-06-05 17:11:01 +0800

    Fix struct tag

--- internal/model/rsvp.go
+++ internal/model/rsvp.go
@@ -3,9 +3,9 @@ package model
 import "github.com/google/uuid"
 
 type RSVP struct {
-	Id             uuid.UUID `json:id`
-	EventId        uuid.UUID `json:eventId`
-	UserId         uuid.UUID `json:userId`
-	Attending      bool      `json:attending`
-	NumberOfGuests int       `json:numberOfGuests`
+	Id             uuid.UUID `json:"id"`
+	EventId        uuid.UUID `json:"eventId"`
+	UserId         uuid.UUID `json:"userId"`
+	Attending      bool      `json:"attending"`
+	NumberOfGuests int       `json:"numberOfGuests"`
 }
